407b82ecad4a51acb2e3a34a2bcf168a62ef8d14
Miaw Code <bararaff@gmail.com>
2024-09-19 14:00:08 +0700

    first commit

---
+++ 3.boolean.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("benar adalah : ", true)
+	fmt.Println("salah adalah : ", false)
+}
---
+++ 4.string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("ini adalah string"))
+	fmt.Println("ini adalah string"[0]) //mengambil huruf pertama, dan menghasilkan value berbentuk number(bit)
+	fmt.Println("ini adalah string"[1])
+
+}
